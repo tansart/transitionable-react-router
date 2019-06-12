88b1129e2b03fb0fc955096a1184d7d62d442288
Thomas <thomas.ansart@stilva.com>
2019-06-11 20:53:34 -0400

    First Router implementation

--- src/Router.js
+++ src/Router.js
@@ -1,3 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import {RouterContext} from './RouterContext';
+
 export function Router({children}) {
-  return children;
+  const [state, setState] = useState({currentRoute: window.location.pathname, previousRoute: ''});
+
+  const setRoute = path => {
+    window.history.pushState({}, null, path);
+
+    setState({
+      currentRoute: window.location.pathname,
+      previousRoute: state.currentRoute
+    });
+  };
+
+  useEffect(() => {
+    function onPopState() {
+      setState({
+        currentRoute: `/${window.location.pathname}`.replace('//', '/'),
+        previousRoute: state.currentRoute
+      });
+    }
+
+    window.addEventListener('popstate', onPopState);
+
+    return function() {
+      window.removeEventListener('popstate', onPopState);
+    }
+  }, []);
+
+  return <RouterContext.Provider value={{setRoute, ...state}}>
+    {children}
+  </RouterContext.Provider>;
 }
